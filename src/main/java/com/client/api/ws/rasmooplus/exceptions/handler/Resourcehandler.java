e01ac33d1674a6dec229095aef63c6345cde80f6
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-08-14 12:07:49 -0300

    Blocking create url to not update

--- src/main/java/com/client/api/ws/rasmooplus/exceptions/handler/Resourcehandler.java
+++ src/main/java/com/client/api/ws/rasmooplus/exceptions/handler/Resourcehandler.java
@@ -1,6 +1,7 @@
 package com.client.api.ws.rasmooplus.exceptions.handler;
 
 
+import com.client.api.ws.rasmooplus.exceptions.BadRequestException;
 import com.client.api.ws.rasmooplus.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,10 @@ public class Resourcehandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleNotFoundException(BadRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
--- src/main/java/com/client/api/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
+++ src/main/java/com/client/api/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.client.api.ws.rasmooplus.service.impl;
 
 import com.client.api.ws.rasmooplus.dto.SubscriptionTypeDTO;
+import com.client.api.ws.rasmooplus.exceptions.BadRequestException;
 import com.client.api.ws.rasmooplus.exceptions.NotFoundException;
 import com.client.api.ws.rasmooplus.model.SubscriptionType;
 import com.client.api.ws.rasmooplus.repository.SubscriptionTypeRepository;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -40,6 +42,11 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
     @Override
     public SubscriptionType create(SubscriptionTypeDTO subscriptionTypeDTO) {
+
+        if(Objects.nonNull(subscriptionTypeDTO.getId())) {
+            throw new BadRequestException("Subscription type id already exists");
+        }
+
         return repository.save(SubscriptionType.builder()
                                                 .id(subscriptionTypeDTO.getId())
                                                 .name(subscriptionTypeDTO.getName())
