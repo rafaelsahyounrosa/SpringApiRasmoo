266506cae9618bf3dada7828645527b5399e0ccd
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-09-08 10:09:40 -0300

    final adjustments

--- src/main/java/com/client/api/ws/rasmooplus/repository/jpa/SubscriptionTypeRepository.java
+++ src/main/java/com/client/api/ws/rasmooplus/repository/jpa/SubscriptionTypeRepository.java
@@ -5,6 +5,10 @@ import com.client.api.ws.rasmooplus.model.jpa.SubscriptionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubscriptionTypeRepository extends JpaRepository<SubscriptionType, Long> {
+
+    Optional<SubscriptionType> findByProductKey(String productKey);
 }
